c81480a0864e5b2a2679afe3632d6d975368b5c5
Edivaldo Coelho <113144617+EdivaldoC@users.noreply.github.com>
2023-05-13 18:24:19 -0300

    primeiro comit

---
+++ Codigos/Animais/Mamiferos.java
@@ -0,0 +1,36 @@
+package Codigos.Animais;
+
+import Codigos.Animal;
+
+public abstract class Mamiferos extends Animal {
+    private int contpelos;
+
+    public Mamiferos(int id, String nome, int idade, float peso, int contpelos) {
+        super(id, nome, idade, peso);
+        this.contpelos = contpelos;
+        String ultimacomida = "";
+
+    }
+
+    @Override
+    public void setAlimento(String ultimacomida) {
+
+        super.setAlimento(ultimacomida);
+
+    }
+
+    @Override
+    public String getAlimento() {
+
+        return super.getAlimento();
+    }
+
+    public void setContpelos(int contpelos) {
+        this.contpelos = contpelos;
+    }
+
+    public int getContpelos() {
+        return contpelos;
+    }
+
+}
